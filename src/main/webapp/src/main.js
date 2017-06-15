269e512246f1cac45ef26b2d0fbd21d7784c135a
Nihel <nihelbengamra@gmail.com>
2017-06-15 16:43:28 +0200

    [develop] vuex installation

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
+import store from '@/stores/indexStore'
 import router from './router'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
---
+++ src/main/webapp/src/stores/indexStore.js
@@ -0,0 +1,22 @@
+/**
+ * Created by NBE3663 on 15/06/2017.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    testStore:{
+      name:'coucou'
+    }
+  },
+  actions: {
+  },
+  mutations: {
+  },
+  getters: {
+  }
+})
+export default store
