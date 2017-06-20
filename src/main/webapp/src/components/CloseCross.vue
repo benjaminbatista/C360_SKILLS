d8edc29a25830ac2f0a64b2c29dca80dfee8479a
HBI1 <hamza.bourakkadi@gmail.com>
2017-06-20 17:14:41 +0200

    [US#GCo12] Suppression de lien

---
+++ src/main/webapp/src/components/CloseCross.vue
@@ -0,0 +1,31 @@
+<template>
+  <g>
+    <line :x1="x1" :y1="y1+12"
+          :x2="x1+12" :y2="y1"
+          stroke="red"
+          stroke-width="3"/>
+    <line :x1="x1" :y1="y1"
+          :x2="x1 + 12" :y2="y1+12"
+          stroke="red"
+          stroke-width="3"/>
+  </g>
+</template>
+
+<script>
+  import store from "../stores/indexStore"
+
+  export default {
+    props: ["x1", "y1"],
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+
+</script>
+
+<style>
+
+
+
+</style>
--- src/main/webapp/src/components/Container-svg.vue
+++ src/main/webapp/src/components/Container-svg.vue
@@ -14,38 +14,43 @@
     <div class="svg-content" id="test">
       <svg version="1.1" viewBox="0 0 1250 1250" preserveAspectRatio="xMinYMin meet">
         <g v-for="link in links">
-          <line   :x1="getPositionXById(link.skill1.id)" :y1="getPositionYById(link.skill1.id)"
-                  :x2="getPositionXById(link.skill2.id)"
-                  :y2="getPositionYById(link.skill2.id)" style="stroke:black;stroke-width:2"/>
+          <line @mouseover="selectedlink = link" :x1="getPositionXById(link.skill1.id)"
+                :y1="getPositionYById(link.skill1.id)"
+                :x2="getPositionXById(link.skill2.id)"
+                :y2="getPositionYById(link.skill2.id)" style="stroke:black;stroke-width:2"/>
         </g>
         <g v-for="(skill,i) in skills">
           <customCircle :id="skill.id" :cx="positionX(i)" :cy="positionY(i)" :content="skill.label"
                         @click="selectSkill(skill)"/>
         </g>
+        <CloseCross :x1="linkPositionX()" :y1="linkPositionY()"></CloseCross>
       </svg>
+
     </div>
   </div>
 </template>
 
 <script>
   import CustomCircle from "./customcircle"
+  import CloseCross from "./CloseCross.vue"
 
   export default {
 //    name: 'container-svg',
     data () {
       return {
         selectedSkill: {
-            skill1:'',
-            skill2:''
+          skill1: '',
+          skill2: ''
         },
         skills: [],
+        selectedlink: '',
         label: '',
         text: [],
         posX: 100,
         posY: 100,
         numberOfCircle: 0,
         row: 0,
-        links:[]
+        links: []
       }
     },
     mounted(){
@@ -53,25 +58,51 @@
       this.getAllLinks();
     },
     methods: {
+      linkPositionX(){
+        if (this.selectedlink != '') {
+          var x1 = parseFloat(this.getPositionXById(this.selectedlink.skill1.id))-3;
+          var x2 = parseInt(this.getPositionXById(this.selectedlink.skill2.id))-3;
+          console.log("x1= " +x1);
+          console.log("x2= "+x2);
+          var total = (x1 + x2) / 2;
+          console.log(total);
+          return total
+        }
+        else
+          return 0
+      },
+      linkPositionY(){
+        if (this.selectedlink != '') {
+          var y1 = parseInt(this.getPositionYById(this.selectedlink.skill1.id)) - 3;
+          var y2 = parseInt(this.getPositionYById(this.selectedlink.skill2.id)) - 3;
+          console.log("y1= " +y1);
+          console.log("y2= "+y2);
+          var somme = y1+y2;
+          console.log(somme/2);
+          return somme/2
+        }
+        else
+          return 0;
+      },
 
       waitForElementToDisplay(selector, time, position){
         let self = this;
-        if(document.getElementById(selector)!=null) {
+        if (document.getElementById(selector) != null) {
           return document.getElementById(selector).getElementsByTagName("circle")[0].getAttribute(position);
         }
         else {
-          setTimeout(function() {
+          setTimeout(function () {
             self.waitForElementToDisplay(selector, time);
           }, time);
         }
       },
 
       getPositionXById(id){
-        return this.waitForElementToDisplay(id,0,"cx");
+        return this.waitForElementToDisplay(id, 0, "cx");
       },
 
       getPositionYById(id){
-       return this.waitForElementToDisplay(id,0,"cy");
+        return this.waitForElementToDisplay(id, 0, "cy");
       },
       selectSkill(skill){
         if (this.selectedSkill.skill1 == '')
@@ -79,15 +110,15 @@
         else {
           this.selectedSkill.skill2 = skill;
           this.$http.post('http://localhost:8083/api/addlink', this.selectedSkill).then(
-          response => {
-                console.log(response);
-          }, response => {
-            console.log(response);
-          }).then(
+            response => {
+              console.log(response);
+            }, response => {
+              console.log(response);
+            }).then(
             function () {
               this.selectedSkill = {
-                skill1:'',
-                skill2:''
+                skill1: '',
+                skill2: ''
               };
               this.getAllSkills();
               this.getAllLinks();
@@ -96,7 +127,7 @@
         }
       },
       positionX(integ){
-          console.log()
+        console.log()
         return this.posX + ((integ) % 8) * 150;
       },
       positionY(integ){
@@ -137,7 +168,7 @@
         });
       }
     },
-    components: {customCircle: CustomCircle}
+    components: {customCircle: CustomCircle, CloseCross: CloseCross}
 
   }
 </script>
